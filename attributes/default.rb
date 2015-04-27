516df3e406ee6d3e41051d4cac547aac14a6694a
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-04-27 10:44:30 +0300

    add bind-utils for host, move packages to attribs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,3 @@
 default['cog_c7common']['timezone'] = 'Europe/Tallinn'
 default['cog_c7common']['motd'] = 'This server is managed by Chef!'
+default['cog_c7common']['packages'] = ['lsof','strace','sysstat','curl','wget','openssh-clients','gcc','make','man','psmisc','ntp','bzip2','iotop','libselinux-python','policycoreutils-python','git','yum-utils','bind-utils']
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,8 +3,11 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-package ['lsof','strace','sysstat','curl','wget','openssh-clients','gcc','make','man','psmisc','ntp','bzip2','iotop','libselinux-python','policycoreutils-python','git','yum-utils'] do
-	action	:install
+
+node['cog_c7common']['packages'].each do |n|
+	package n do
+		action	:install
+	end
 end
 
 timezone = node['cog_c7common']['timezone']
